25965f94a7d404347ae507d6cb6c56122cf93c96
Matheus Duarte <matheusdcarvalho007@gmail.com>
2019-06-06 19:08:44 -0300

    add routeProvider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-const app = angular.module('tutorialAngular', ["tutorialController"]); 
+var app = angular.module('tutorialAngular', ["ngRoute", "tutorialController"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+
+        .when("/", {
+            templateUrl: "../views/home.html"
+        })
+        .when("/tutorial", {
+            templateUrl: "views/tutorial.html",
+            controller: "TutorialController"
+        })
+
+})
